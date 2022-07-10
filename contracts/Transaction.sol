5e68346783a5d98a018ba32fbfa340b7b973d44f
arichan <yuki.ariyama.biz@gmail.com>
2022-07-11 07:42:56 +0900

    add countract

---
+++ contracts/Transaction.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// I could't import to other files. Try later.
+
+// pragma solidity ^0.8.4;
+
+// import "hardhat/console.sol";
+
+// contract Transaction {
+//     address senderAddress;
+//     // address receiverAddress;
+
+//     constructor(address senderAddress){
+//         console.log("I am Transaction.");
+//         senderAddress = senderAddress;
+//         // receiverAddress = receiverAddress;
+//     }
+
+// }
